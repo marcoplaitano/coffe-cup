63f94c0b148cf6073e5c741332003f40a9d8a887
Marco Plaitano <marcoplaitano23@gmail.com>
2024-11-11 16:51:22 +0100

    Add delete user button

--- server.js
+++ server.js
@@ -53,6 +53,16 @@ app.post("/api/add-user", async (req, res) => {
   res.json(sortedData); // Send the sorted list to the client
 });
 
+// API endpoint to delete a user
+app.post("/api/delete-user", async (req, res) => {
+  let data = await readJSONFile(FILENAME);
+  const { name } = req.body;
+  delete data[name];
+  await writeJSONFile(FILENAME, data);
+  let sortedData = Object.entries(data).sort((a, b) => b[1] - a[1]);
+  res.json(sortedData); // Send the sorted list to the client
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
